27898e51120c92be00fc955d683d62c4525c189a
Andrew Nowak <andrew@nowak.me.uk>
2024-10-17 10:43:48 +0100

    add support for .java-version file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::env;
+use std::fs;
+use std::process::exit;
 use std::process::Command;
+use std::{env, path::Path};
 
 use serde::Deserialize;
 use tabled::{settings::Style, Table, Tabled};
@@ -29,27 +31,27 @@ impl Jvm {
     fn major_version(&self) -> u16 {
         let version = self.version.clone();
         let (major_version, rest) = version.split_once('.').unwrap_or_else(|| {
-            panic!(
+            exit_with_err(&format!(
                 "Version number {} of jvm {} should contain at least one period!",
                 self.version, self.home_path
-            )
+            ))
         });
 
         let major_version = match major_version {
             "1" => rest.split_once('.').unwrap_or_else(|| {
-                panic!(
+                exit_with_err(&format!(
                     "Version number {} of jvm {} should contain at least two periods when 1-prefixed!",
                     self.version, self.home_path
-                )
+                ))
             }).0,
             otherwise => otherwise,
         };
 
         major_version.parse::<u16>().unwrap_or_else(|_| {
-            panic!(
+            exit_with_err(&format!(
                 "Major version number {} of JVM {} should be numeric!",
                 major_version, self.home_path
-            )
+            ))
         })
     }
 
@@ -107,6 +109,25 @@ fn switch_to(spec: &str, jvms: &[Jvm]) {
     }
 }
 
+fn find_from_file(dir: &Path) -> String {
+    let jv_file = dir.join(".java-version");
+    if fs::exists(jv_file.clone()).unwrap() {
+        let contents = fs::read_to_string(jv_file).unwrap();
+        contents.trim().to_string()
+    } else if let Some(parent) = dir.parent() {
+        find_from_file(parent)
+    } else {
+        exit_with_err(
+            "No .java_version file found in this directory or any parent!",
+        );
+    }
+}
+
+fn exit_with_err(msg: &str) -> ! {
+    eprintln!("{}", msg);
+    exit(1)
+}
+
 fn main() {
     let java_home_in = Command::new("/usr/libexec/java_home")
         .arg("-X")
@@ -122,6 +143,14 @@ fn main() {
 
     match args.get(1) {
         None => list_all(&jvms),
+        Some(spec) if spec == "use" => {
+            // TODO be quiet if "switching" to same jdk
+            let here = Path::new(".")
+                .canonicalize()
+                .expect("?? Couldn't find the path to this directory? What?");
+            let spec = find_from_file(&here);
+            switch_to(&spec, &jvms)
+        }
         Some(spec) => switch_to(spec, &jvms),
     }
 }
